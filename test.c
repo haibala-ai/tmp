1bad76147139fd37b969f6da45c8c392c87db71e
sherry <wu12345@mail.ustc.edu.cn>
2024-09-25 16:25:20 +0800

    ok1

--- test.c
+++ test.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    printf("Hello world");
+    printf("fuck you apex!");
     return 0;
 }
---
+++ test2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a
+    float a=0;
+    scanf("%f",&a);
+    printf("%f",a+1);
+    return 0;
+}
